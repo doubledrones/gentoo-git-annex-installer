d6190c8c0cc0294fd6bbd94b9d97c5441f89f05a
Marcin Nowicki <pr0d1r2@gmail.com>
2011-11-26 17:52:31 +0100

    Revert monad-control

--- install-git-annex.sh
+++ install-git-annex.sh
@@ -40,7 +40,7 @@ add_keywords_line ">=dev-haskell/json-0.5 ~amd64"
 add_keywords_line "#required by dev-haskell/monad-control-0.2.0.3, required by =dev-haskell/monad-control-0.2.0.3 (argument)"
 add_keywords_line ">=dev-haskell/transformers-0.2.2.0 ~amd64"
 add_keywords_line "#required by =dev-haskell/monad-control-0.2.0.3 (argument)"
-add_keywords_line "=dev-haskell/monad-control-9999 ~amd64"
+add_keywords_line "=dev-haskell/monad-control-0.2.0.3 ~amd64"
 add_keywords_line "#required by dev-haskell/monad-control-0.2.0.3, required by =dev-haskell/monad-control-0.2.0.3 (argument)"
 add_keywords_line ">=dev-haskell/base-unicode-symbols-0.2.2.2 ~amd64"
 
@@ -58,5 +58,5 @@ add_keywords_line "#required by dev-vcs/git-annex-9999, required by git-annex (a
 add_keywords_line ">=dev-haskell/pcre-light-0.4 ~amd64"
 
 emerge -v =dev-haskell/syb-0.3.3 && \
-emerge -v =dev-haskell/monad-control-9999 && \
+emerge -v =dev-haskell/monad-control-0.2.0.3 && \
 emerge -v =dev-vcs/git-annex-9999
